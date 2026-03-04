3680c68b3e12733ab7fe86a344d7619b932ff9a6
desumidhun2006 <desumidhun2006@gmail.com>
2026-03-04 11:26:52 +0530

    Black key addition

--- js/piano.js
+++ js/piano.js
@@ -1,21 +1,31 @@
 const pianoContainer = document.getElementById("piano")
 
 const keyMap = [
-    {note:"C4", key:"a"},
-    {note:"D4", key:"s"},
-    {note:"E4", key:"d"},
-    {note:"F4", key:"f"},
-    {note:"G4", key:"g"},
-    {note:"A4", key:"h"},
-    {note:"B4", key:"j"}
+    {note:"C4", key:"a", type:"white"},
+    {note:"C#4", key:"w", type:"black"},
+    {note:"D4", key:"s", type:"white"},
+    {note:"D#4", key:"e", type:"black"},
+    {note:"E4", key:"d", type:"white"},
+    {note:"F4", key:"f", type:"white"},
+    {note:"F#4", key:"t", type:"black"},
+    {note:"G4", key:"g", type:"white"},
+    {note:"G#4", key:"y", type:"black"},
+    {note:"A4", key:"h", type:"white"},
+    {note:"A#4", key:"u", type:"black"},
+    {note:"B4", key:"j", type:"white"}
 ]
 
 const keys = []
 
-keyMap.forEach(item => {
+keyMap.forEach((item,index)=>{
 
     const key = document.createElement("div")
-    key.classList.add("key")
+
+    if(item.type === "black"){
+        key.classList.add("key","black-key")
+    }else{
+        key.classList.add("key")
+    }
 
     key.dataset.note = item.note
     key.dataset.key = item.key
@@ -25,7 +35,7 @@ keyMap.forEach(item => {
         <span class="kbd">${item.key.toUpperCase()}</span>
     `
 
-    key.addEventListener("click", () => {
+    key.addEventListener("click",()=>{
         playNote(item.note)
     })
 
