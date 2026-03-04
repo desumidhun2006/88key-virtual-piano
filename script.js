4c31112fd0a9f7096e7161b430a6b3fddcb853a0
desumidhun2006 <desumidhun2006@gmail.com>
2026-03-04 11:15:29 +0530

    some thing i have done

--- script.js
+++ script.js
@@ -2,9 +2,55 @@ const synth = new Tone.Synth().toDestination();
 
 const keys = document.querySelectorAll(".key");
 
+function playNote(note){
+    synth.triggerAttackRelease(note,"8n");
+}
+
 keys.forEach(key => {
+
     key.addEventListener("click", () => {
-        const note = key.dataset.note;
-        synth.triggerAttackRelease(note, "8n");
+        playNote(key.dataset.note);
     });
+
 });
+
+document.addEventListener("keydown", (e) => {
+
+    keys.forEach(key => {
+
+        if(e.key === key.dataset.key){
+            playNote(key.dataset.note);
+        }
+
+    });
+
+});
+
+document.addEventListener("keydown",(e)=>{
+
+    keys.forEach(key=>{
+
+        if(e.key === key.dataset.key){
+
+            key.classList.add("active")
+            playNote(key.dataset.note)
+
+        }
+
+    })
+
+})
+
+document.addEventListener("keyup",(e)=>{
+
+    keys.forEach(key=>{
+
+        if(e.key === key.dataset.key){
+
+            key.classList.remove("active")
+
+        }
+
+    })
+
+})
